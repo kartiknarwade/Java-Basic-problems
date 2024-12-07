bc9b87187fc8556aeffbba7528276caa63d06966
Kartik <kartiknarwade6@gmail.com>
2024-12-07 21:47:18 +0530

    7/12/2024

--- CompareNM.java
+++ CompareNM.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
+class CompareNM {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t;
